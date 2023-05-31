c2e4a7e5b11db4f6e0da961e285686a1cea95f7f
thangcai <nguyenhoangthang546@gmail.com>
2023-05-31 17:17:48 +0700

    create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    ConfirmationComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
